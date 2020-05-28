6d0844192c0086caeaef6cf513672df6c0e6b7e1
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2020-05-28 11:01:13 -0700

    Add files via upload

---
+++ src/rta/YellowAddr.java
@@ -0,0 +1,21 @@
+package rta;
+
+public class YellowAddr {
+    public static final int joypadAddr = 0x01B9;
+    public static final int joypadOverworldAddr = 0x0C51;
+    public static final int newBattleAddr = 0x0480;
+    public static final int enterMapAddr = 0x01D7;
+    public static final int encounterTestAddr = 0x04788E;
+    public static final int saveInjectAddr = 0x1C79D6;
+    public static final int pikaInjectAddr = 0x1C79E7;
+    public static final int delayAtEndOfShootingStarAddr = 0x105A74;
+    public static final int softResetAddr = 0x1D05;
+    public static final int intro2Addr = 0x3E596A;
+    public static final int intro4Addr = 0x3E5A1E;
+    public static final int intro6Addr = 0x3E5A6B;
+    public static final int intro8Addr = 0x3E5AD8;
+    public static final int intro10Addr = 0x3E5B04;
+    public static final int intro12Addr = 0x3E5CAC;
+    public static final int titleScreenAddr = 0x014171;
+    public static final int noYesAddr = 0x074245;
+}
