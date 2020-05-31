1b660eb7a2bcd437795e4835a6c39c917112f846
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2020-05-30 17:10:55 -0700

    Add files via upload

---
+++ src/rta/CrystalAddr.java
@@ -0,0 +1,34 @@
+package rta;
+
+public class CrystalAddr {
+    public static final int readJoypadAddr = 0x098F;
+    public static final int titleResetAddr = 0x637E;
+    public static final int introScene0Addr = 0x39465E;
+    public static final int introScene1Addr = 0x39495B;
+    public static final int introScene3Addr = 0x3949FD;
+    public static final int introScene5Addr = 0x394A7A;
+    public static final int introScene7Addr = 0x394B3F;
+    public static final int introScene11Addr = 0x394C86;
+    public static final int introScene13Addr = 0x394D6D;
+    public static final int introScene15Addr = 0x394E40;
+    public static final int introScene17Addr = 0x394EF5;
+    public static final int introScene19Addr = 0x394F7E;
+    public static final int introScene26Addr = 0x3950BB;
+    public static final int titleScreenAddr = 0x436D67;
+    public static final int mainMenuJoypadAddr = 0x125DF5;
+    public static final int disableLcdAddr = 0x0568;
+    public static final int initAddr = 0x017D;
+    public static final int postLID = 0x015D5F;
+    public static final int noYesBoxAddr = 0x135569;
+
+    public static final int owPlayerInputAddr = 0x256974;
+    public static final int countStepAddr = 0x256B79;
+    public static final int startWildBattleAddr = 0x0A60F6;
+    public static final int calcStatsAddr = 0x03617A;
+    public static final int randomAddr = 0x2F8C;
+
+    public static final int printLetterDelayAddr = 0x3168;
+    public static final int bonkSoundAddr = 0x2043EE;
+    public static final int doPlayerDamageAddr = 0x0D5099;
+    public static final int seenByTrainerAddr = 0x25686E;
+}
