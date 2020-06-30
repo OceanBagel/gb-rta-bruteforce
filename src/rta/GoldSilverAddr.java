dc06def0edd6c5822a82e9fe67f4bf9b1a9539b8
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2020-06-29 21:05:13 -0700

    Add files via upload

---
+++ src/rta/GoldSilverAddr.java
@@ -0,0 +1,11 @@
+package rta;
+
+public class GoldSilverAddr {
+	public static final int postTIDAddr = 0x015C6D;
+//	public static final int initAddr = 0x05D1;
+	public static final int readJoypadAddr = 0x0940;
+	public static final int mainMenuJoypadAddr = 0x5B13;
+    
+//	public static final int goldTitleScreenAddr = 0x016434;
+//	public static final int silverTitleScreenAddr = 0x016432;
+}
